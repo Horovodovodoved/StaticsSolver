d39cddcb08b2b03ce2c7049370ef34a1a6e6e1c6
Юрий <brovchenko.iuo@phystech.edu>
2023-04-14 11:08:50 +0300

    Доделал Solver (почти), Task и Force. Полностью сделал логику добавления уравнений в матрицы в классе Solver. Наметил логику метода checkEquationsNumber. Заменил четыре класса, отвечающих за информацию о силах и их проекциях, на один - Force. Сделал три нужных конструктора в классе Force и новый набор полей, в том числе идентификаторы проекций. Добавил новый класс HingedConnection, массив объектов которого хранится в классе Task. Переработал класс Task до нужного вида: всё, что нужно, и ничего лишнего.

--- src/Solver.java
+++ src/Solver.java
@@ -1,6 +1,79 @@
+import java.util.ArrayList;
+
 public class Solver {
+  Task task;
+  ArrayList<double[]> A = new ArrayList<>();
+  ArrayList<double[]> b = new ArrayList<>();
   
-  public static Answer getSolution(Task task) {
-    
+  public Solver(Task task) {
+    this.task = task;
+    makeMatrixEquation();
+  }
+  
+  public Answer getAnswer() {
+    return null;
+  }
+  
+  // We have 5 types of equations
+  // We need as many equations, as unknown variables
+  // We are mathematicians
+  private void makeMatrixEquation() {
+    addXEquations();
+    addYEquations();
+    addMEquations();
+    addKnownAngleEquations();
+    addHingedConnectionEquations();
+    checkEquationsNumber();
+  }
+  
+  private void addXEquations() {
+    for (Body body : task.getBodies()) {
+      A.add(body.getXCoefs());
+      b.add(body.getXLeftConst()); // "left" because it's before "=" mark
+    }
+  }
+  
+  private void addYEquations() {
+    for (Body body : task.getBodies()) {
+      A.add(body.getYCoefs());
+      b.add(body.getYLeftConst());
+    }
+  }
+  
+  private void addMEquations() {
+    for (Body body : task.getBodies()) {
+      A.add(body.getMCoefs());
+      b.add(body.getMLeftConst());
+    }
+  }
+  
+  private void addKnownAngleEquations() {
+    for (Body body : task.getBodies()) {
+      A.addAll(body.getKnownAngleCoefs());
+    }
+    for (int i = 0; i < A.size() - b.size(); i++) {
+      b.add(new double[]{0});
+    }
+  }
+  
+  private void addHingedConnectionEquations() {
+    for (HingedConnection hc : task.getHingedConnections()) {
+      A.add(hc.getXCoefs());
+      A.add(hc.getYCoefs());
+      b.add(new double[]{0});
+      b.add(new double[]{0});
+    }
+  }
+  
+  private void checkEquationsNumber() {
+    //todo
+    // если больше:
+    //   проверить на совместность
+    //   если совместна:
+    //     выкинуть лишние
+    //   иначе:
+    //     выдать ошибку
+    // если меньше:
+    //   выдать ошибку
   }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,102 +1,62 @@
 import java.util.ArrayList;
 
 public class Task {
+  private int variables_num;
   private ArrayList<Body> bodies = new ArrayList<>();
-  private int num_variables = 0; // projections; needless
-  @Deprecated
-  private ArrayList<UnknownForce> unknown_forces = new ArrayList<>();
-  @Deprecated
-  private ArrayList<UnknownForceProjection> unknown_projections = new ArrayList<>();
+  private ArrayList<HingedConnection> hinged_connections = new ArrayList<>();
   
-  public void addBody() {
-    bodies.add(new Body());
+  public void setBodies(int num) {
+    for (int i = 0; i < num; i++) {
+      bodies.add(new Body());
+    }
+  }
+  
+  public ArrayList<Body> getBodies() {
+    return bodies;
   }
 
-  /*
-  Новые метод  с использованием одного класса
-   */
-
-  public void addExternalForce_(String id, int body_idx, Point point,
-                               Double value, Double angle) {
-    KnownForce force = new KnownForce(id, body_idx, point, value, angle);
-
-    bodies.get(body_idx).addForce(force);
+  public void addKnownForce(int body_index, Point point,
+                            double value, double angle) {
+    bodies.get(body_index).addForce(new Force(body_index, point, value, angle));
+  }
+  
+  public void addUnknownForceWithKnownAngle(int body_index, Point point,
+                                     double angle) {
+    bodies.get(body_index).addForce(new Force(body_index, point, angle,
+        variables_num + 1, variables_num + 2));
+    variables_num += 2;
+  }
+  
+  public void addUnknownForce(int body_index, Point point) {
+    bodies.get(body_index).addForce(new Force(body_index, point,
+        variables_num + 1, variables_num + 2));
+    variables_num += 2;
   }
 
-  public void addExternalForce_(String id, int body_idx, Point point) {
-    KnownForce force = new KnownForce(id, body_idx, point, null, null);
-
-    bodies.get(body_idx).addForce(force);
+  public void addHingedConnection(int body_1_index, int body_2_index,
+                                  Point point) {
+    Force force_1 = new Force(body_1_index, point,
+        variables_num + 1, variables_num + 2);
+    Force force_2 = new Force(body_2_index, point,
+        variables_num + 3, variables_num + 4);
+    bodies.get(body_1_index).addForce(force_1);
+    bodies.get(body_2_index).addForce(force_2);
+    variables_num += 4;
+    hinged_connections.add(new HingedConnection(force_1, force_2));
   }
 
-  public void addHingedConnection(String id_1, String id_2,
-                                  int body_1_idx, int body_2_idx, Point point) {
-    addExternalForce_(id_1, body_1_idx, point);
-    addExternalForce_(id_2, body_2_idx, point);
-    // todo: Каким-то образом оставить в id_1 и id_2 информацию для решателя
-    //  о том, что
-    //  force_1 = -force_2
-    num_variables += 2;
+  public void addHingedSupport(int body_index, Point point) {
+    addUnknownForce(body_index, point);
   }
 
-  public void addHingedSupport(String id, int body_idx, Point point) {
-    addExternalForce_(id, body_idx, point);
-  }
-
-  public void addSmoothSupport(String id, int body_idx,
+  public void addSmoothSupport(int body_index,
                                Point point, double normal_angle) {
-    addExternalForce(id, body_idx, point, null, normal_angle);
+    addUnknownForceWithKnownAngle(body_index, point, normal_angle);
   }
-
-
-  /*
-  Далее - устаревшие методы
-   */
-  @Deprecated
-  public void addExternalForce(String id, int body_idx, Point point,
-                               Double value, double angle) {
-    KnownForce force = new KnownForce(id, body_idx, point, value, angle);
-    bodies.get(body_idx).known_forces.add(force);
-    KnownForceProjection x_projection =
-        new KnownForceProjection(force, "x");
-    KnownForceProjection y_projection =
-        new KnownForceProjection(force, "y");
-    bodies.get(body_idx).x_known_map.put(x_projection.id, x_projection);
-    bodies.get(body_idx).y_known_map.put(y_projection.id, y_projection);
+  
+  public ArrayList<HingedConnection> getHingedConnections() {
+    return hinged_connections;
   }
-  @Deprecated
-  public void addUnknownExternalForce(String id, int body_idx, Point point) {
-    UnknownForce force = new UnknownForce(id, body_idx, point);
-    unknown_forces.add(force);
-    num_variables += 2;
-    
-    bodies.get(body_idx).unknown_forces.add(force);
-    UnknownForceProjection x_projection =
-        new UnknownForceProjection(force, "x");
-    UnknownForceProjection y_projection =
-        new UnknownForceProjection(force, "y");
-    bodies.get(body_idx).x_unknown_map.put(x_projection.id, x_projection);
-    bodies.get(body_idx).y_unknown_map.put(y_projection.id, y_projection);
-    unknown_projections.add(x_projection);
-    unknown_projections.add(y_projection);
-  }
-  @Deprecated
-  public void addUnknownExternalForce(String id, int body_idx, Point point,
-                                      boolean isAbsoluteValueKnown,
-                                      double known_param) {
-    UnknownForce force = new UnknownForce(id, body_idx, point,
-        isAbsoluteValueKnown, known_param);
-    unknown_forces.add(force);
-    num_variables += 1;
-    
-    bodies.get(body_idx).unknown_forces.add(force);
-    UnknownForceProjection x_projection =
-        new UnknownForceProjection(force, "x");
-    UnknownForceProjection y_projection =
-        new UnknownForceProjection(force, "y");
-    bodies.get(body_idx).x_unknown_map.put(x_projection.id, x_projection);
-    bodies.get(body_idx).y_unknown_map.put(y_projection.id, y_projection);
-    unknown_projections.add(x_projection);
-    unknown_projections.add(y_projection);
-  }
+  
+  public int getVariablesNum() { return variables_num; }
 }
